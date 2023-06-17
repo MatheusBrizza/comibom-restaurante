d080b40426ddd81e7ad29cb477dc0d082f191e49
Matheus <matheusbj95@gmail.com>
2023-06-17 20:24:08 -0300

    first commit

---
+++ src/main/java/com/comibomrestaurante/ComibomRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.comibomrestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComibomRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComibomRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comibomrestaurante/ComibomRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comibomrestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComibomRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
